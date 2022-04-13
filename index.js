7f0ddb6537103df847c92c28b98ca24fe720aafa
Ayush Mhetre <mhetreayush1719@gmail.com>
2022-04-14 01:59:15 +0530

    Search bar functionality added

--- index.js
+++ index.js
@@ -1,82 +1,77 @@
 const products = document.querySelector(".placeHolder");
 
-function createProd([img1, name1, img2, name2, img3, name3]) {
+function createProd(img, name) {
     let code = `
     <div class="layout">
-            <img src="${img1}" alt="">
-            <h1 style="font-family: 'Bebas Neue', cursive;">${name1}</h1>
+            <img src="${img}" alt="">
+            <h1 style="font-family: 'Bebas Neue', cursive;">${name}</h1>
             <h6>Description</h6>
-    </div>
-    <div class="layout">
-        <img src="${img2}" alt="">
-        <h1 style="font-family: 'Bebas Neue', cursive;">${name2}</h1>
-        <h6>Description</h6>
-    </div>
-    <div class="layout">
-        <img src="${img3}" alt="">
-        <h1 style="font-family: 'Bebas Neue', cursive;">${name3}</h1>
-        <h6>Description</h6>
     </div>
     `;
     products.innerHTML += code;
 }
 
-let item1 = [
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnnnObTCNg1QJoEd9Krwl3kSUnPYTZrxb5Ig&usqp=CAU",
-    "Orange",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnnnObTCNg1QJoEd9Krwl3kSUnPYTZrxb5Ig&usqp=CAU",
-    "Orange",
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnnnObTCNg1QJoEd9Krwl3kSUnPYTZrxb5Ig&usqp=CAU",
-    "Orange"
+let item1 = {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTnnnObTCNg1QJoEd9Krwl3kSUnPYTZrxb5Ig&usqp=CAU",
+    name: "Orange"
+};
 
-];
+let item2 = {
+    img: "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
+    name: "Apple"
+};
 
-let item2 = [
-    "https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bananas-218094b-scaled.jpg",
-    "Banana",
-    "https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bananas-218094b-scaled.jpg",
-    "Banana",
-    "https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bananas-218094b-scaled.jpg",
-    "Banana"
-
-];
-
-let item3 = [
-    "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
-    "Apple",
-    "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
-    "Apple",
-    "https://www.collinsdictionary.com/images/full/apple_158989157.jpg",
-    "Apple"
-
-];
+let item3 = {
+    img: "https://images.immediate.co.uk/production/volatile/sites/30/2017/01/Bananas-218094b-scaled.jpg",
+    name: "Banana"
+};
 for (i = 0; i < 5; i++) {
-    createProd(item1);
-    createProd(item2);
-    createProd(item3);
+    createProd(item1.img, item1.name);
+    createProd(item2.img, item2.name);
+    createProd(item3.img, item3.name);
 }
 
-function getColors(){
-    url="https://x-colors.herokuapp.com/api/random";
-    fetch(url).then((response)=>{
+function getColors() {
+    url = "https://x-colors.herokuapp.com/api/random";
+    fetch(url).then((response) => {
         return response.json();
-    }).then((data=>{
-        let bgcol=data.hex;
+    }).then((data => {
+        let bgcol = data.hex;
         document.body.style.backgroundColor = `${bgcol}`;
-        document.querySelector("title").innerHTML="BG-Color : " + `${bgcol}`;
-        
+        document.querySelector("title").innerHTML = "BG-Color : " + `${bgcol}`;
+
     }))
-    fetch(url).then((response)=>{
+    fetch(url).then((response) => {
         return response.json();
-    }).then((data=>{
-        let layoutCol=data.hex;
+    }).then((data => {
+        let layoutCol = data.hex;
         var numOfLayout = document.querySelectorAll(".layout").length;
-        for (i=0; i<numOfLayout; i++){
-        document.querySelectorAll(".layout")[i].style.backgroundColor=layoutCol;
-    }}))
+        for (i = 0; i < numOfLayout; i++) {
+            document.querySelectorAll(".layout")[i].style.backgroundColor = layoutCol;
+        }
+    }))
 
     setTimeout(getColors, 1500);
 }
 
 getColors()
 
+const searchInput = document.getElementById("search");
+
+searchInput.addEventListener("input", e => {
+    
+    var numOfLayout = document.querySelectorAll(".layout").length;
+    
+    for (i=0; i<numOfLayout; i++){
+        const value = e.target.value;
+        var check = document.querySelectorAll(".layout")[i].querySelector("h1").innerHTML;
+    console.log(i)
+    console.log(check.toLocaleLowerCase().includes(value.toLocaleLowerCase()))
+    if(check.toLocaleLowerCase().includes(value.toLocaleLowerCase())==false){
+        document.querySelectorAll(".layout")[i].style.display="none"
+    }
+    else if(value == ""){
+        window.location.reload();
+    }
+    }
+})
